2052f2ad57d473ed969d0ff74f09c8740f61a69f
Flipize <nilsson.filip@hotmail.se>
2019-05-18 10:49:23 +0200

    Book Class

---
+++ src/Book.java
@@ -0,0 +1,44 @@
+public class Book {
+    private int isbn;
+    private String title;
+    private int count;
+
+    public Book() {
+    }
+
+    public Book(int isbn, String title, int count) {
+        this.isbn = isbn;
+        this.title = title;
+        this.count = count;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String toString() {
+        String name = "";
+        name = "Title: " + this.title + " ISBN: " + this.isbn + " Count: " + this.count;
+        return name;
+    }
+}
