82babddd353b0e6bd588d62fe9d3143de29b3329
Flipize <nilsson.filip@hotmail.se>
2019-05-18 09:54:50 +0200

    test

---
+++ src/HelloTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hi() {
+    }
+}
