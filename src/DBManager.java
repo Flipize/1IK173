349866be4cd424237c2db2dfd260c28182180f99
Flipize <nilsson.filip@hotmail.se>
2019-05-21 16:23:49 +0200

    Merge branch 'master' of C:\Users\nilss\IdeaProjects\1IK173 with conflicts.

--- src/DBManager.java
+++ src/DBManager.java
@@ -2,11 +2,11 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class DBManager {
-    private static String password = "xxx";
+    private static String password = "eldorado5";
     private static String driver = "jdbc:mysql://localhost/library?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     //private static String driver = "jdbc:mysql://localhost/Library?useSSL=false";
 
-    public static ArrayList<Member> getMemberArrayList() {
+    private static ArrayList<Member> getMemberArrayList() {
         ArrayList<Member> memberArrayList = new ArrayList<>();
 
         try {
@@ -24,7 +24,7 @@ public class DBManager {
                     "SELECT * FROM Member");
 
             while (rs_member.next()) {
-                memberArrayList.add(new Member(rs_member.getInt(1), rs_member.getString(2), rs_member.getString(3)));
+                memberArrayList.add(new Member(rs_member.getInt(1), rs_member.getString(2), rs_member.getString(4)));
 
             }
 
@@ -35,7 +35,7 @@ public class DBManager {
         return memberArrayList;
     }
 
-    public static ArrayList<Book> getBookArrayList() {
+    private static ArrayList<Book> getBookArrayList() {
         ArrayList<Book> bookArrayList = new ArrayList<>();
 
         try {
@@ -64,27 +64,57 @@ public class DBManager {
         return bookArrayList;
     }
 
-    public static ArrayList <Book> addBook (int id, int isbn, String title) {
+    public static void main(String[] args) {
+        ArrayList<Member> m_array = getMemberArrayList();
+        for (Member m : m_array) {
+            System.out.println(m.toString());
+        }
 
-        ArrayList <Book> bookArrayList = new ArrayList<>();
+        ArrayList<Book> b_array = getBookArrayList();
+        for (Book b : b_array) {
+            System.out.println(b.toString());
+        }
 
+        ArrayList<String[]> l_array = getLoanArrayList();
+        for (String[] s : l_array){
+            System.out.println(s[0] + " " + s[1] + " " + s[2] + " " + s[3]);
+        }
+    }
+
+    private static ArrayList<String[]> getLoanArrayList() {
+        ArrayList<String[]> loanArrayList = new ArrayList<>();
+        String[] temp = new String[4];
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Driver did not load");
+        }
         try (Connection conn = DriverManager.getConnection(
-                driver, "root" , password)) {
+                driver, "root", password)) {
             System.out.println("Connected");
 
-            PreparedStatement statement = conn.prepareStatement("INSERT INTO Book VALUES (?, ?, ?)");
-            statement.setInt(1, id);
-            statement.setInt(2, isbn);
-            statement.setString(3, title);
+            Statement statement = conn.createStatement();
+            ResultSet rs_loans = statement.executeQuery(
+                    "SELECT * FROM loan");
 
-            statement.executeUpdate();
-            bookArrayList.add(new Book(id, isbn, title));
+            while (rs_loans.next()) {
+                temp[0] = rs_loans.getString(1);
+                temp[1] = rs_loans.getString(2);
+                temp[2] = rs_loans.getString(3);
+                temp[3] = rs_loans.getString(4);
+
+                loanArrayList.add(temp);
+                temp = new String[4];
+
+            }
 
         } catch (SQLException ex) {
             System.out.println("Something went wrong..." + ex.getMessage());
         }
 
-        return bookArrayList;
+        return loanArrayList;
     }
 
 
@@ -143,19 +173,6 @@ public class DBManager {
             System.out.println("Something went wrong..." + ex.getMessage());
         }
 
-    }
-
-
-    public static void main(String[] args) {
-        ArrayList<Member> m_array = getMemberArrayList();
-        for (Member m : m_array) {
-            System.out.println(m.toString());
-        }
-
-        ArrayList<Book> b_array = getBookArrayList();
-        for (Book b : b_array) {
-            System.out.println(b.toString());
-        }
     }
 
 }
