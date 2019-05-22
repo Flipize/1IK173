e3113b54626ccbe759fa4b5ad6ae356a46ef915c
beigart <beigart94@gmail.com>
2019-05-22 18:38:05 +0200

    DBMANAGER

--- src/libraryManager.java
+++ src/libraryManager.java
@@ -1,3 +1,4 @@
+import java.sql.Date;
 import java.util.ArrayList;
 
 import static java.lang.Integer.parseInt;
@@ -5,8 +6,10 @@ import static java.lang.Integer.parseInt;
 public class libraryManager {
 
     public static void main(String[] args) {
-        Member newMember = regApplicant("Bran the Broken");
-        checkRegistration(newMember);
+        //Member newMember = regApplicant("Bran the Broken");
+        //checkRegistration(newMember);
+
+        lendItem(5, 100001);
     }
 
     public boolean isBookAvailable(String bookTitle) {
@@ -86,11 +89,12 @@ public class libraryManager {
 
     }
 
-    public static void lendItem (int id, String bookName) {
+    public static void lendItem (int id, int isbn) {
 
         ArrayList<Member> members = DBManager.getMemberArrayList();
         ArrayList<Book> books = DBManager.getBookArrayList();
 
+        //Kollar om medlemmen finns
         for (Member m : members) {
             if (m.getId() == id) {
                 System.out.println("Member found");
@@ -100,13 +104,48 @@ public class libraryManager {
 
         }
 
+        //Kollar om medlemmen får låna fler böcker
         for (Member m : members) {
             if (m.getMembershipType().equals("Undergraduate")) {
+                if (DBManager.loanCount(id) > 3) {
+                    System.out.println("Cannot borrow any more books");
+                }
             }
+            else if (m.getMembershipType().equals("Masterstudent")) {
+                if (DBManager.loanCount(id) > 5) {
+                    System.out.println("Cannot borrow any more books");
+                }
+            }
+            else if (m.getMembershipType().equals("PhD")) {
+                if (DBManager.loanCount(id) > 7) {
+                    System.out.println("Cannot borrow any more books");
+                }
+            }
+            else if (m.getMembershipType().equals("Teacher")) {
+                if (DBManager.loanCount(id) > 10) {
+                    System.out.println("Cannot borrow any more books");
+                }
+            }
+            else
+                System.out.println("Borrowing book allowed");
         }
 
 
-    }
+        for (Book b : books){
+            if (b.getIsbn() == isbn){
+                if (b.isAvailable()) {
+                    System.out.println("Book available");
+                    DBManager.addLoan(b.getId(), id, Date.valueOf("20190403"), Date.valueOf("20190413"));
+                    System.out.println("Congratulations, you have borrowed the book");
+                }
+                else
+                    System.out.println("Book is currently not availble, please come again");
 
+            }
+            else
+                System.out.println("Book doesn't exist");
+        }
+
+    }
 
 }
