bde33638568bbc6ac9547333588b411d4f104a5e
Flipize <nilsson.filip@hotmail.se>
2019-05-25 18:31:32 +0200

    member class

--- src/LibraryManagerTest.java
+++ src/LibraryManagerTest.java
@@ -1,30 +1,23 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import java.util.ArrayList;
 
-import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class LibraryManagerTest {
-    DBManager dbMock;
-    ArrayList<Book> bookAL;
-
-    public LibraryManagerTest(DBManager dbManager) {
-        dbMock = dbManager;
-    }
-
-    @BeforeAll
-    static void setUp() {
-        ArrayList<Book> bookAL = new ArrayList<>();
-        bookAL.add(new Book(1, 1234, "Book", true));
-    }
+    DBManager dbMock = mock(DBManager.class);
+    ArrayList<Book> bookAL = new ArrayList<>();
 
     @Test
     void isBookAvailable() {
         when(dbMock.getBookArrayList()).thenReturn(bookAL);
-        assertEquals(true, isBookAvailable(1234));
+        assertEquals(true, LibraryManager.isBookAvailable(1234));
     }
 
     @Test
