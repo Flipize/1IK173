a241e8e10ae4b02884d56247ce1145e2dc5ab922
Flipize <nilsson.filip@hotmail.se>
2019-05-25 17:14:07 +0200

    member class

---
+++ src/libraryManagerTest.java
@@ -0,0 +1,88 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class libraryManagerTest {
+
+    @BeforeAll
+    void setUp() {
+    }
+
+    @Test
+    void isBookAvailable() {
+    }
+
+    @Test
+    void returnBook() {
+    }
+
+    @Test
+    void regApplicant() {
+    }
+
+    @Test
+    void deleteMemberLibrary() {
+    }
+
+    @Test
+    void lendItem() {
+    }
+
+    @Test
+    void getRandId() {
+    }
+
+    @Test
+    void idIsValid() {
+    }
+
+    @Test
+    void isBanned() {
+    }
+
+    @Test
+    void ban() {
+    }
+
+    @Test
+    void getMemberById() {
+    }
+
+    @Test
+    void suspendMember() {
+    }
+
+    @Test
+    void isMemberIn() {
+    }
+
+    @Test
+    void getMemberByPN() {
+    }
+
+    @Test
+    void isSuspensionIn() {
+    }
+
+    @Test
+    void addMember() {
+    }
+
+    @Test
+    void checkIfExistingMember() {
+    }
+
+    @Test
+    void addBook() {
+    }
+
+    @Test
+    void getLibrarian() {
+    }
+
+    @Test
+    void validLibrarian() {
+    }
+}
