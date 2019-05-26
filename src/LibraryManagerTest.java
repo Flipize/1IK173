555d8353016672ca8a122bc04dcdb43b605709ae
Flipize <nilsson.filip@hotmail.se>
2019-05-26 15:31:08 +0200

    member class

--- src/LibraryManagerTest.java
+++ src/LibraryManagerTest.java
@@ -5,6 +5,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,16 +13,34 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class LibraryManagerTest {
+    public ArrayList<Book> books = new ArrayList<>();
+    public ArrayList<Member> members = new ArrayList<>();
+    public ArrayList<Librarian> librarians = new ArrayList<>();
+    public ArrayList<Suspension> suspensions = new ArrayList<>();
+    public ArrayList<String[]> loans = new ArrayList<>();
+
+
+    public LibraryManagerTest() {
+        books.add(new Book(1,1010101,"Game of Thrones", true));
+        books.add(new Book(2,2020202,"Game of Phonies", false));
+        books.add(new Book(3,3030303,"Game of Bros", true));
+        members.add(new Member(1, "Flipize", 12891212L, "Student"));
+        members.add(new Member(2, "Gurr", 12891212L, "Techer"));
+        members.add(new Member(1, "Beggan", 12891212L, "PhD Student"));
+        librarians.add(new Librarian(1234, "Gunilla Andersson"));
+        librarians.add(new Librarian(5678, "Alfred Persson"));
+        suspensions.add(new Suspension(3,3, LocalDate.parse ("1330-12-21"), LocalDate.parse("2019-05-30")));
+        loans.add(new String[]{"2", "2", "20190526", "20190603"});
+
+    }
 
     @Test
-    void testOst() {
+   void testOst() {
         DBManager db = mock(DBManager.class);
         LibraryManager lm = new LibraryManager(db);
-        ArrayList<Book> bookAL = new ArrayList<>();
-        bookAL.add(new Book(7, 7766, "Hejsan", false));
-        bookAL.add(new Book(8, 7765, "Hejsan", true));
+        ArrayList<Book> bookAL = books;
         when(db.getBookArrayList()).thenReturn(bookAL);
-        assertTrue(lm.isBookAvailable(7766));
+        assertTrue(lm.isBookAvailable(1010101));
     }
 
     @Test
